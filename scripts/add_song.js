ee1da3135156f7f60a31d1176a5c0df38e0c0096
Aung Aung <aaung8@ivytech.edu>
2025-02-24 22:07:55 -0500

    adding crud functionality to frontend

---
+++ scripts/add_song.js
@@ -0,0 +1,33 @@
+addEventListener("DOMContentLoaded", function(){
+    document.querySelector("#addBtn").addEventListener("click", addSong)
+})
+
+async function addSong(){
+    const song = {
+        title: document.querySelector("#title").value,
+        artist: document.querySelector("#artist").value,
+        releaseDate: document.querySelector("#released").value,
+        popularity: document.querySelector("#popularity").value,
+        genre: document.querySelector("#genre").value ? document.querySelector("#genre").value.split(",") : [] // split up into an array
+    }
+
+    const response = await fetch("http://localhost:3000/api/songs",{ 
+        method: "POST",
+        headers:{
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(song)
+    })
+
+    if(response.ok){
+        const results = await response.json()
+        // hidden param of _id
+        alert("Added song with ID of" + results._id)
+
+        //reset form
+        document.querySelector("form").reset()
+    }
+    else{
+        document.querySelector("#error").innerHtml = "Cannot Add Song"
+    }
+}
