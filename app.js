d158903bce4138b2a4392bd4348bb1fdb91194ed
Aung Aung <aaung8@ivytech.edu>
2025-02-24 20:57:54 -0500

    added db

--- app.js
+++ app.js
@@ -1,43 +1,75 @@
-const express = require("express");
-var cors = require("cors")
-//activate or tell this app variable to be an express server
-
-const app = express()
+const express =  require("express");
 // we have to use cors in order to host a front end and backend on the same device
-app.use(cors())
-const router = express.Router()
+var cors = require('cors')
 
+const bodyParser = require('body-parser')
+const Song = require("./models/song")
+const app = express();
+app.use(cors());
 
-//listening function
-app.listen(3000, function() {
-    console.log("Listening on port 3000");
-})
-
-// all requests that usually use an api start with /api... the url would be api/songs
-app.use("/api", router)
+app.use(express.json())
  
-//making an api using routes
-//routes are used to handle browser request
-router.get("/songs", function(req, res) {
-    const song = [{
-        title: "Uptown Funk",
-        artist: "Bruno Mars",
-        popularity: 10,
-        releaseDate: new Date(2021, 9, 20),
-        genre: ["funk", "boogie"]
-    }, {
-        title: "Valentine",
-        artist: "Laufey",
-        popularity: 9,
-        releaseDate: new Date(2021, 9, 20),
-        genre: ["jazz", "pop"]
-    }, {
-        title: "Magnolia",
-        artist: "Playboy Carti",
-        popularity: 11,
-        releaseDate: new Date(2021, 9, 20),
-        genre: ["rap", "vampire"]
-    }]
-    res.json(song)
+const router = express.Router();
+
+//grab all the songs in database
+router.get("/songs", async(req,res) =>{
+   try{
+      const songs = await Song.find({})
+      res.send(songs)
+      console.log(songs)
+   }
+   catch (err){
+      console.log(err)
+   }
 })
 
+router.get("/songs/:id", async (req, res) => {
+   try{
+      const song = await Song.findById(req.params.id)
+      res.json(song)
+   }
+   catch(err){
+      res.status(400).send(err)
+   }
+})
+
+
+router.post("/songs", async(req,res) =>{
+   try{
+      const song = await new Song(req.body)
+      await song.save()
+      res.status(201).json(song)
+      console.log(song)
+   }
+   catch(err){
+      res.status(400).send(err)
+   }
+})
+
+router.put("/songs/:id", async(req, res) =>{
+   try{
+      const song = req.body
+      await Song.updateOne({_id: req.params.id},song)  
+      console.log(song)
+      res.sendStatus(204)
+   }
+   catch(err){
+      res.status(400).send(err)  
+   }
+})
+
+router.delete("/songs/:id", async(req, res) =>{
+   try{
+      const song = req.body
+      await Song.deleteOne({_id: req.params.id},song)  
+      console.log(song)
+      res.sendStatus(204)
+   }
+   catch(err){
+      res.status(400).send(err)  
+   }
+})
+
+// all requests that usually use an api start with /api... so the url woul be localhost:3000/api/songs
+app.use("/api", router);
+app.listen(3000);
---
+++ db.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://sdev255:Aung2007@songdb.9vody.mongodb.net/?retryWrites=true&w=majority&appName=SongDB")
+
+module.exports = mongoose
